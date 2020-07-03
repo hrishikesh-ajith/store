915fe7fb35b220a992f6a2fa4a655b194f32e216
hrishikesh-ajith <hrishikeshajith0@gmail.com>
2020-07-04 01:24:45 +0530

    3rd commit ,signup screen

--- app/src/main/java/com/example/store/frontscreen.java
+++ app/src/main/java/com/example/store/frontscreen.java
@@ -2,18 +2,31 @@ package com.example.store;
 
         import androidx.appcompat.app.AppCompatActivity;
 
+        import android.content.Intent;
         import android.os.Bundle;
+        import android.view.View;
         import android.view.WindowManager;
         import android.view.animation.Animation;
         import android.view.animation.AnimationUtils;
+        import android.widget.Button;
         import android.widget.ImageView;
         import android.widget.TextView;
 
 public class frontscreen extends AppCompatActivity {
-
+    Button callsignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        callsignup=findViewById(R.id.button3);
+
+        callsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(frontscreen.this,Signup.class);
+                startActivity(intent);
+            }
+        });
     }
 }
