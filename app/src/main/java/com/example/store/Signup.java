08a412be757f996615e3a93c6857b46b3ea36da3
hrishikesh-ajith <hrishikeshajith0@gmail.com>
2020-10-30 03:02:07 +0530

    registration commit 1

--- app/src/main/java/com/example/store/Signup.java
+++ app/src/main/java/com/example/store/Signup.java
@@ -3,12 +3,39 @@ package com.example.store;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Signup extends AppCompatActivity {
+
+ EditText regName, regUsername, regEmail, regPhoneNo, regPassword;
+ Button regBtn;
+ FirebaseDatabase rootNode;
+ DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        //hooks
+        regName=findViewById(R.id.reg_fullname);
+        regUsername=findViewById(R.id.reg_username);
+        regEmail=findViewById(R.id.reg_email);
+        regPhoneNo=findViewById(R.id.reg_phone);
+        regPassword=findViewById(R.id.reg_password);
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rootNode = FirebaseDatabase.getInstance();
+
+            }
+        });
+
     }
 }
