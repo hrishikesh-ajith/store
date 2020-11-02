1d9e96f83a3f93e0aee84b509a285c1e75663499
hrishikesh-ajith <hrishikeshajith0@gmail.com>
2020-11-02 22:33:29 +0530

    idk

--- app/src/main/java/com/example/store/frontscreen.java
+++ app/src/main/java/com/example/store/frontscreen.java
@@ -1,5 +1,6 @@
 package com.example.store;
 
+        import androidx.annotation.NonNull;
         import androidx.appcompat.app.AppCompatActivity;
 
         import android.content.Intent;
@@ -9,36 +10,69 @@ package com.example.store;
         import android.view.animation.Animation;
         import android.view.animation.AnimationUtils;
         import android.widget.Button;
+        import android.widget.EditText;
         import android.widget.ImageView;
         import android.widget.TextView;
+        import android.widget.Toast;
+
+        import com.google.firebase.auth.FirebaseAuth;
+        import com.google.firebase.database.DataSnapshot;
+        import com.google.firebase.database.DatabaseError;
+        import com.google.firebase.database.DatabaseReference;
+        import com.google.firebase.database.FirebaseDatabase;
+        import com.google.firebase.database.Query;
+        import com.google.firebase.database.ValueEventListener;
 
 public class frontscreen extends AppCompatActivity {
     Button callsignup;
-    Button callhome;
+    Button btnlogin;
+   private EditText username,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        callsignup=findViewById(R.id.button3);
-
-        callsignup.setOnClickListener(new View.OnClickListener() {
+        username=findViewById(R.id.log_username);
+        password=findViewById(R.id.log_password);
+        btnlogin=findViewById(R.id.button);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent =new Intent(frontscreen.this,Signup.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                String usernamefromuser=username.getText().toString();
+                final String passwordfromuser=password.getText().toString();
+
+
+                if(usernamefromuser.isEmpty() || passwordfromuser.isEmpty())
+                {
+                    Toast.makeText(frontscreen.this,"Enter something you Dumbfuck",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+                    Query checkUser =reference.orderByChild("username").equalTo(usernamefromuser);
+                    checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            String passwordfromDB=dataSnapshot.child(passwordfromuser).child("password").getValue(String.class);
+                            if(passwordfromDB.equals(passwordfromuser))
+                            {
+                                Intent intent=new Intent(frontscreen.this,homescreen.class);
+                                startActivity(intent);
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+
             }
         });
 
-       callhome=findViewById(R.id.button);
-        callhome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent =new Intent(frontscreen.this,homescreen.class);
-                startActivity(intent);
-            }
-        });
+
 
     }
+
+
 }
