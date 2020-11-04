2b307c290d6389f6df05c96a3b2994ad80e88d06
srlkmiV <srlkmi5@gmail.com>
2020-11-04 15:22:45 +0530

    campusselect kengeri

--- app/src/main/java/com/example/store/campusselect.java
+++ app/src/main/java/com/example/store/campusselect.java
@@ -2,7 +2,9 @@ package com.example.store;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class campusselect extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class campusselect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campusselect);
+    }
+
+    public void openkengeri(View view) {
+        Intent send = new Intent(campusselect.this, shop_list_keng.class);
+        startActivity(send);
     }
 }
